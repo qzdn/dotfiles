8711addb78f35ea7ed4552e00b674a5712bf2144
qzdn <6467022+qzdn@users.noreply.github.com>
2023-12-09 19:48:32 +0300

    new tools+aur yay

--- tools.sh
+++ tools.sh
@@ -6,7 +6,13 @@
 # ttf-jetbrains-mono, ttf-font-awesome - fonts
 # pavucontrol, playerctl - volume and media buttons control
 # mangohud, goverlay - game monitoring
-# telegram-desktop, discord, steam - entertainments
+# telegram-desktop, discord, steam, yt-dlp, streamlink - entertainments
 # 
+# AUR
+# yay - package manager
+# yewtube - yt in terminal
+# chatterino2-git - twitch chat
 
-sudo pacman -S alacritty picom rofi mc pcmanfm feh ttf-jetbrains-mono ttf-font-awesome pavucontrol playerctl mangohud goverlay telegram-desktop discord steam
+sudo pacman -S alacritty picom rofi mc pcmanfm feh ttf-jetbrains-mono ttf-font-awesome pavucontrol playerctl mangohud goverlay telegram-desktop discord steam yt-dlp streamlink
+
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
